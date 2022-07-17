67dee0bc479b42c4456d33b49eb6f9f864a6f70e
seekment <ylyang@email.swu.edu.cn>
2022-07-17 21:57:13 +0800

    扩展方式注入 String 引用对象 数组 map set Properties

--- spring-04-di/src/test/java/MyTest.java
+++ spring-04-di/src/test/java/MyTest.java
@@ -16,7 +16,12 @@ public class MyTest {
         System.out.println("注入开始");
         ApplicationContext ApplicationContext = new ClassPathXmlApplicationContext("beans.xml");
         Student student = (Student)ApplicationContext.getBean("student");
+
         System.out.println(student.getName());
+        System.out.println(student.getAddresss());
+        System.out.println(student.getBooks());
+        System.out.println(student.getCard());
+        System.out.println(student.getGames());
         System.out.println("注入结束");
     }
 }
