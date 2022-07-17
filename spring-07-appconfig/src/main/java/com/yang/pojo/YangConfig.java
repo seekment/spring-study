b6e0f7cb059da65bf70d5758a4a66a00d5f0acff
seekment <ylyang@email.swu.edu.cn>
2022-07-17 23:51:07 +0800

    纯Java实现自动装配，AppConfig方式 不需要任何的xml IOC容器文件了

--- spring-07-appconfig/src/main/java/com/yang/pojo/YangConfig.java
+++ spring-07-appconfig/src/main/java/com/yang/pojo/YangConfig.java
@@ -10,9 +10,10 @@ import org.springframework.context.annotation.Configuration;
  * @Date 22/07/17 23:43
  * @Version v1.0
  **/
-
+// 和xml配置文件比较类型
 @Configuration
 public class YangConfig {
+    // 定义一个bean
     @Bean
     public User getUser(){
         return new User();
